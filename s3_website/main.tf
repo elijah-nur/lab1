3d84d57e0831d6f19db9f1162885cb6e4770f24c
elijah-nur <iliyataran18@gmail.com>
2025-05-11 13:36:15 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -1,72 +1,65 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = "~> 4.0"
+}
+}
 }
 
+# Configure AWS provider and creds
 provider "aws" {
-  region = "us-east-1"
-
+region = "us-east-1"
 }
 
+# Creating bucket
 resource "aws_s3_bucket" "website" {
-  bucket = "my-buck-elijah-202505111237"
+bucket = "buackettrrrrrvasdf"
 
-  tags = {
-    Name        = "Website"
-    Environment = "Dev"
-  }
+tags = {
+Name = "Website"
+Environment = "Dev"
 }
-
-resource "aws_s3_bucket_public_access_block" "website" {
-  bucket = aws_s3_bucket.website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
 }
-
+resource "aws_s3_bucket_acl" "example_acl" {
+bucket = aws_s3_bucket.website.id
+acl = "public-read"
+}
 resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.website.id
-  index_document {
-    suffix = "index.html"
-  }
-  error_document {
-    key = "error.html"
-  }
+bucket = aws_s3_bucket.website.id
+index_document {
+suffix = "index.html"
 }
-
+error_document {
+key = "error.html"
+}
+}
 resource "aws_s3_bucket_policy" "allow_access" {
-  bucket = aws_s3_bucket.website.id
-  policy = data.aws_iam_policy_document.allow_access.json
+bucket = aws_s3_bucket.website.id
+policy = data.aws_iam_policy_document.allow_access.json
 }
-
 data "aws_iam_policy_document" "allow_access" {
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    actions = [
-      "s3:GetObject"
-    ]
-    resources = [
-      "${aws_s3_bucket.website.arn}/*"
-    ]
-  }
+statement {
+principals {
+type = "AWS"
+identifiers = ["*"]
 }
-
+actions = [
+"s3:GetObject"
+]
+resources = [
+aws_s3_bucket.website.arn,
+"${aws_s3_bucket.website.arn}/*",
+]
+}
+}
 resource "aws_s3_object" "indexfile" {
-  bucket       = aws_s3_bucket.website.id
-  key          = "index.html"
-  source       = "./src/index.html"
-  content_type = "text/html"
+bucket = aws_s3_bucket.website.id
+key = "index.html"
+source = "./src/index.html"
+content_type = "text/html"
 }
 
 output "website_endpoint" {
-  value = aws_s3_bucket_website_configuration.website_config.website_endpoint
+value = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
